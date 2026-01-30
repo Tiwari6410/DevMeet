7184f0f8559d207a0033904a39f3753a09da2ed7
Tiwari6410 <arvind.tiwari6410.gmail.com>
2026-01-30 16:17:34 +0530

    nested routes handle

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
-// console.log("hello are you available for talk")
-
 const express = require("express")
-const app = express()
+const app = express();
 
 
-
-app.get("/hello",(req,res)=>{
-    res.send("i am from hello routes")
+//middleware 
+app.use("/", (req, res, next)=>{
+    next()
 })
 
-app.get("/timba", (req,res)=>{
-    res.send("i am from timba server")
+app.get("/user", (req, res, next)=>{
+    console.log("i am from 1st request hander")                      //middleware
+    next();
+}, (req, res, next)=>{                                              //middleware 
+    console.log("i am from second request handler ")
+    next()
+}, (req, res, next)=>{
+    res.send("this is the final response of the request handler ") //request handler 
+    next()
 })
 
-app.use("/",(req, res)=>{
- res.send("hello i am from server")
+app.listen("7777", ()=>{
+    console.log("program is suppose to done and finish this is the last") 
 })
-app.listen(3000,()=>{
-    console.log("congratulation our server hasbeen successfully started")
 
-})
+
+
+
+
+// // console.log("hello are you available for talk")
+
+// const express = require("express")
+// const app = express()
+
+
+
+// app.get("/hello",(req,res)=>{
+//     res.send("i am from hello routes")
+// })
+
+// app.get("/timba", (req,res)=>{
+//     res.send("i am from timba server")
+// })
+
+// app.use("/",(req, res)=>{
+//  res.send("hello i am from server")
+// })
+// app.listen(3000,()=>{
+//     console.log("congratulation our server hasbeen successfully started")
+
+// })
+
+
+
